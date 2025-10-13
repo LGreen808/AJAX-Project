4ead59492243de8168353d9f7d6544ae8796d2d9
LGreen808 <526LGreen@frhsd.com>
2025-10-13 23:40:06 +0000

    8th commit

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-document.getElementById("load").addEventListener("click", function(){
+document.getElementById("load1").addEventListener("click", function(){
  const xhr = new XMLHttpRequest(); // Creates a new request
- xhr.open("GET","data.JSON", true); // Prepares the request above
+ xhr.open("GET","data.json", true); // Prepares the request above
     xhr.onload = function(){
         if (xhr.status === 200) {
             const membersArray = JSON.parse(xhr.responseText);
             console.log(membersArray);
-            document.getElementById("output").innerHTML = membersArray.map(members => 
+            document.getElementById("output1").innerHTML = membersArray.map(members => 
                 `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
             ).join('');
         }
     };
-    xhr.send();
+    document.getElementById("load2").addEventListener("click", function(){
+ const xhr = new XMLHttpRequest(); // Creates a new request
+ xhr.open("GET","data.json", true); // Prepares the request above
+    xhr.onload = function(){
+        if (xhr.status === 200) {
+            const membersArray = JSON.parse(xhr.responseText);
+            console.log(membersArray);
+            document.getElementById("output2").innerHTML = membersArray.map(members => 
+                `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
+            ).join('');
+        }
+    };
+document.getElementById("load3").addEventListener("click", function(){
+ const xhr = new XMLHttpRequest(); // Creates a new request
+ xhr.open("GET","data.json", true); // Prepares the request above
+    xhr.onload = function(){
+        if (xhr.status === 200) {
+            const membersArray = JSON.parse(xhr.responseText);
+            console.log(membersArray);
+            document.getElementById("output3").innerHTML = membersArray.map(members => 
+                `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
+            ).join('');
+        }
+    };
+    
 });
