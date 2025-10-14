5125604a8356afa57bff9f650c90a8b1e5219480
LGreen808 <526LGreen@frhsd.com>
2025-10-14 01:29:40 +0000

    9th commit

--- script.js
+++ script.js
@@ -1,38 +1,14 @@
-document.getElementById("load1").addEventListener("click", function(){
- const xhr = new XMLHttpRequest(); // Creates a new request
- xhr.open("GET","data.json", true); // Prepares the request above
-    xhr.onload = function(){
+document.getElementById("load").addEventListener("click", function () {
+    const xhr = new XMLHttpRequest(); // Creates a new request
+    xhr.open("GET", "data.json", true); // Prepares the request above
+    xhr.onload = function () {
         if (xhr.status === 200) {
             const membersArray = JSON.parse(xhr.responseText);
-            console.log(membersArray);
-            document.getElementById("output1").innerHTML = membersArray.map(members => 
-                `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
+            document.getElementById("output").innerHTML = membersArray.map(member =>
+                `<p><strong>${member.name}</strong> - ${member.relationship} - ${member.contact} - ${member.hobbies.join(", ")} - (${member.age} AGE)</p>`
             ).join('');
         }
-    };
-    document.getElementById("load2").addEventListener("click", function(){
- const xhr = new XMLHttpRequest(); // Creates a new request
- xhr.open("GET","data.json", true); // Prepares the request above
-    xhr.onload = function(){
-        if (xhr.status === 200) {
-            const membersArray = JSON.parse(xhr.responseText);
-            console.log(membersArray);
-            document.getElementById("output2").innerHTML = membersArray.map(members => 
-                `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
-            ).join('');
-        }
-    };
-document.getElementById("load3").addEventListener("click", function(){
- const xhr = new XMLHttpRequest(); // Creates a new request
- xhr.open("GET","data.json", true); // Prepares the request above
-    xhr.onload = function(){
-        if (xhr.status === 200) {
-            const membersArray = JSON.parse(xhr.responseText);
-            console.log(membersArray);
-            document.getElementById("output3").innerHTML = membersArray.map(members => 
-                `<p><strong>${members.name}</strong> - ${members.relationship} - ${members.contact} - ${members.hobbies} - (${members.age} AGE)</p>`
-            ).join('');
-        }
-    };
     
+    };
+     xhr.send();
 });
